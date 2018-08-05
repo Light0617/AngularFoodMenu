a4f49d901d54c260c9f3697ebb0872f0afb6f8b2
light0617 <light11470617@gmail.com>
2018-08-05 13:55:43 -0700

    REST Save

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
 
 @Component({
@@ -14,10 +15,28 @@ import { DishService } from '../services/dish.service';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishcopy = null;
   dishIds: number[];
   prev: number;
   next: number;
+  comment: Comment;
   errMess: string;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Author is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.'
+    }
+  };
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -25,15 +44,16 @@ export class DishdetailComponent implements OnInit {
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
+
+    this.createForm();
+
     this.dishservice.getDishIds()
         .subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-          .pipe(switchMap(
-            (params: Params) => this.dishservice.getDish(+params['id'])
-              )
-          )
-        .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id);},
+          .pipe(switchMap((params: Params) => this.dishservice.getDish(+params['id'])))
+          .subscribe(dish => {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id);},
                    errmess => this.errMess = <any>errmess.message);
+
   }
 
   setPrevNext(dishId: number){
@@ -44,6 +64,49 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  createForm(): void{
+      this.commentForm = this.fb.group({
+        author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+        comment: ['', Validators.required],
+      });
+      this.commentForm.valueChanges
+                      .subscribe(date => this.onValueChanged(data));
+      this.onValueChanged();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISString();
+    console.log(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishcopy.save()
+                 .subscribe(dish => { this.dish = dish; console.log(this.dish); });
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
   }
 
 }
