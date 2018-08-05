09395f9c2f4f1eef1286b056b66dd425bca7295b
light0617 <light11470617@gmail.com>
2018-08-04 23:57:32 -0700

    err handling

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -26,8 +26,7 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getDishIds()
-        .subscribe(dishIds => this.dishIds = dishIds,
-                   errmess => this.errMess = <any>errmess);
+        .subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
           .pipe(switchMap(
             (params: Params) => this.dishservice.getDish(+params['id'])
