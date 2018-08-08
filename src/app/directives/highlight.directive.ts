ab565c6147bacb4d86383bc43cfa717325dd2a27
light0617 <light11470617@gmail.com>
2018-08-07 21:03:24 -0700

    appHighLight

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef,
+              private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.rederer.addClass(this.el.nativeElement, 'highlight');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.rederer.removeClass(this.el.nativeElement, 'highlight');
+  }
 
 }
