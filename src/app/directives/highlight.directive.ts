0540dc24418e137c674351c3b9ca7654aaa7477b
light0617 <light11470617@gmail.com>
2018-08-07 21:35:32 -0700

    add animation

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -9,11 +9,11 @@ export class HighlightDirective {
               private renderer: Renderer2) { }
 
   @HostListener('mouseenter') onMouseEnter(){
-    this.rederer.addClass(this.el.nativeElement, 'highlight');
+    this.renderer.addClass(this.el.nativeElement, 'highlight');
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-    this.rederer.removeClass(this.el.nativeElement, 'highlight');
+    this.renderer.removeClass(this.el.nativeElement, 'highlight');
   }
 
 }
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,7 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { Feedback, ContactType } from '../shared/feedback';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Dish } from '../shared/dish';
 import { Comment } from '../shared/comment';
@@ -12,7 +13,20 @@ import { DishService } from '../services/dish.service';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('1s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -25,6 +39,7 @@ export class DishdetailComponent implements OnInit {
   errMess: string;
   commentForm: FormGroup;
   newComment: Comment;
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -58,10 +73,13 @@ export class DishdetailComponent implements OnInit {
 
     this.dishservice.getDishIds()
         .subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params
-          .pipe(switchMap((params: Params) => this.dishservice.getDish(+params['id'])))
-          .subscribe(dish => {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id);},
-                   errmess => this.errMess = <any>errmess.message);
+    // this.route.params
+    //       .pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+    //       .subscribe(dish => {this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown';},
+    //                errmess => this.errMess = <any>errmess.message);
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+                   .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+                     errmess => this.errMess = <any>errmess);
 
   }
 
